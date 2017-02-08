cb64ccf557c847ff1020e460ef288576f15e4173
mocap <mocap>
2017-02-08 18:14:03 -0500

    Chill work guys

--- ECSE425/cache.vhd
+++ ECSE425/cache.vhd
@@ -31,60 +31,132 @@ architecture arch of cache is
 
 -- declare signals here
 -- I dont know if we actually need all these signals
-signal index: std_logic_vector (4 downto 0);
-signal block_offset: std_logic_vector (1 downto 0);
-signal tag: std_logic_vector (5 downto 0);
+signal c_index: std_logic_vector (4 downto 0);
+signal c_block_offset: std_logic_vector (3 downto 0);
+signal c_tag: std_logic_vector (3 downto 0);
 signal valid: std_logic;
 signal dirty: std_logic;
-signal tag_success: std_logic;
+signal tag_success: std_logic; --implies a hit
+signal word_ptr : INTEGER;
 
 begin
 
 -- make circuits here
 
---Compares the adress tag to the tag in cache (untested)
-procedure compare_tag (tag_adress, tag_cache: in std_logic_vector (5 downto 0); outcome: out std_logic) is 
-begin
-	
-	--Might need to do bitwise comparison
-	if (tag_adress = tag_cache)
-	{
-		outcome <= '1';
-	}
-	else 
-	{
-		outcome = '0'
-	}
-	end if;
-	
-end procedure;
-
-	
 -- 2D Array for storage
 --There are 32 arrays of 4 words (32 bits in length)
-type words is array (3 downto 0) of std_logic_vector (31 downto 0);
-type rows is array  (31 downto 0) of std_logic_vector of words;
+--type words is array (3 downto 0) of std_logic_vector (31 downto 0);
+--type rows is array  (31 downto 0) of std_logic_vector of words;
+
+-- blk[133] = valid
+-- blk[132] = dirty
+-- blk[128-131] = tag
+-- blk[96-127] = WORD1 
+-- blk[64-95] = WORD2
+-- blk[32-63] = WORD3
+-- blk[0-31] = WORD4
+
+type blk is array (1 downto 0) of std_logic_vector (133 downto 0);
+type cache is array (31 downto 0) of std_logic_vector of blk;
+signal cache_array : cache;
 	
 process(clock)
-{
+
+	begin
 	--Set all signals for each clock cycle
 	tag_success <= 0;
-	block_offset <= s_addr (1 downto 0);
-	index <= s_addr (6 downto 2);
-	tag <= s_addr (12 downto 7);
-	dirty <= s_addr (13);
-	valid <= s_addr (14);
-	
-	if(s_read)
-	{
+	c_block_offset <= s_addr (3 downto 0);
+	c_index <= s_addr (8 downto 4 );
+	c_tag <= s_addr (12 downto 9);
+
+        case state is
+
+   		when A =>
+ 			for i in cache_array range loop
+      				cache_array(i,133) <= '0';
+   			end loop;
+			state <= B;
+     			
+   		when B =>
+			if (s_read = '1') then
+				state <= C;
+				
+			elsif (s_write = '1') then
+				state <= F;
+			end if;
+
+
+		--Verifying tag state, getting valid/invalid tag, valid and dirty
+      		when C => 
+			
+			compare_tag(c_index,c_tag,tag_success,valid,dirty);
+			
+			--If valid is 0 and dirty is 0 that means that its a read miss, have to read from memory
+			--The block was not modified, hence safe to go and replace it 
+			if (valid = '0' or dirty = '0' ) then
+				state <= D;
+
+			--If dirty is 1 that means that its a read miss, but 
+			--block was modified. In order to get a block from memory and evict current one
+			--we need to write back the dirty block to memory first
+			elsif (dirty = '1') then
+				state <= G;
+			
+			--Tag is matched! Return the block immediately to cpu
+			elsif (tag_success = '1') then
+				get_word(c_index,c_block_offset,s_readdata);
+				state <= B;
+			end if;
+
 		
-	}
-	
-	if(s_write)
-	{
+      		when D => 
+			if (m_waitrequest = '0') then
+				state <= E;
+
+			elsif (m_waitrequest = '1') then
+				state <= D;
+			end if;
+
+
+		when E => 
+			--REPLACING BLOCK HERE AND THEN GOING BACK TO B
+			state <= B;
+
+
+     		when F => 
+			if (tag_success = '1') then
+				state <= I;
+			else	
+				state <= G;
+			end if;
+
+    		when G => 
+			if (s_read = '1') then
+				state <= D;
+				
+			elsif (s_write = '1') then
+				state <= H;
+
+			elsif (m_waitrequest = '1') then
+				state <= G;
+			end if;
 		
-	}
-}
+      		when H => 
+			if (m_waitrequest = '0') then
+				state <= I;
+
+			elsif (m_waitrequest = '1') then
+				state <= H;
+			end if;
+
+		when I => 
+			--REPLACING BLOCK HERE AND THEN GOING BACK TO B
+			state <= B;
+
+	
+	end case;
+end process;
+
 
 procedure check_dirty_bits is
 begin
@@ -103,7 +175,7 @@ begin
 --TODO
 
 end write_main_mem;
-
+c_index
 
 procedure write_to_cache is
 begin
@@ -111,10 +183,44 @@ begin
 
 end write_to_cache;
 
-procedure read_from_cache is
-begin
---TODO
 
-end read_from_cache;
+procedure compare_tag (c_index: in std_logic_vector (4 downto 0); 
+		     c_tag: in std_logic_vector (3 downto 0);
+		     outcome,valid,dirty: out std_logic) is
+begin
+	for i in cache_array range loop
+      		if (i = to_integer(unsigned(c_index))) then
+
+			if (cache_array(133) = '1') then
+				valid <= '1';
+				
+				if(cache_array(131 downto 128) = c_tag) then
+					outcome <= '1';
+				else
+					outcome <='0';
+				end if;
+
+				dirty <= cache_array(132);
+			else
+				valid <= '0';	
+				outcome <='0';
+				dirty <='0';
+			end if;
+		end if;
+	end loop;
+
+end compare_tag;
+
+--Getting the word from the case based on index and offset. Returning a full word
+procedure get_word (c_index: in std_logic_vector (4 downto 0); 
+		    c_block_offset: in std_logic_vector (3 downto 0);
+		    s_readdata: out std_logic_vector (31 downto 0)) is
+begin
+	
+	word_ptr = 127 - 32 * to_integer(unsigned(c_block_offset(3 downto 2));
+	s_readdata <= cache_array(to_integer(unsigned(c_index)),(word_ptr downto word_ptr - 31));
+
+end get_word;
+
 
 end arch;
