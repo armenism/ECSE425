2283ee697c8dbca63f64fa5141a1a95981465b50
Snazzythat <roman.andoni@gmail.com>
2017-03-15 02:42:53 -0400

    Roman and Armen: added control signals to MEM stage, added pipe process, TODO: actual memory reading/writing and forwarding ALU

--- ECSE425_deliverable4/MEM_STAGE.vhd
+++ ECSE425_deliverable4/MEM_STAGE.vhd
@@ -24,12 +24,62 @@ entity MEM_STAGE is
     EM_STAGE_CONTROL_SIGNALS: in MEM_CTRL_SIGS;
 		WB_STAGE_CONTROL_SIGNALS: in WB_CTRL_SIGS;
 
+    --TODO: Memory module inputs
+
+
     --STAGE OUTPUTS
     --Data read from memory
-    data_read_from_memory: in std_logic_vector(31 downto 0);
-    destination_reg_RD_out: in std_logic_vector (4 downto 0);
-    WB_STAGE_CONTROL_SIGNALS_out: in WB_CTRL_SIGS;
+    data_out: out std_logic_vector(31 downto 0);
+    destination_reg_RD_out: out std_logic_vector (4 downto 0);
+
+    --To be passed to WB stage
+    WB_STAGE_CONTROL_SIGNALS_out: out WB_CTRL_SIGS
 
   );
 
 end entity;
+
+
+architecture arch of MEM_STAGE is
+
+  --Will map to memory data or the ALU output bypassing the memory
+	signal intermediate_data_out : std_logic_vector (31 downto 0);
+
+BEGIN
+
+  -------------------------------------------------------------PROCESSES
+  ------Actual stage process
+  MEM_STAGE_PROCESS : process (clk, reset)
+
+  	begin
+
+  		if reset = '1' then
+
+        data_out <= (others => '0');
+        destination_reg_RD_out <= (others => '0');
+  			WB_STAGE_CONTROL_SIGNALS_out <= (others => '0');
+
+  		elsif rising_edge(clk) then
+
+  			if rdy = '1' then
+
+          data_out <= intermediate_data_out;
+          destination_reg_RD_out <= destination_reg_RD;
+          WB_STAGE_CONTROL_SIGNALS_out <= WB_STAGE_CONTROL_SIGNALS;
+
+  			end if;
+
+      end if;
+
+  end process;
+
+
+  ------Process to read/write to/from memory
+  MEM_OPERATION_PROCESS : process (clk, reset)
+
+  	begin
+
+    --TODO: read from memory and then assign the result to the intermediate_data_out signal.
+   end process;
+
+end arch;
